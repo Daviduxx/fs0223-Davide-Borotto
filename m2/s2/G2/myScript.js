41134d8c7abcddc5ece278990667bd93331d53ca
Daviduxx <borotto.davide@gmail.com>
2023-05-03 17:37:30 +0200

    esercizio 3-4

--- m2/s2/G2/myScript.js
+++ m2/s2/G2/myScript.js
@@ -1,17 +1,27 @@
 let viaggiSaldi = document.querySelectorAll('#saldiCollapse .card');
 let viaggiSummer = document.querySelectorAll('#summerCollapse .card')
 let lastCards = document.querySelectorAll('#lastCards div');
-let numeroViaggi = viaggiSaldi.length + viaggiSummer.length + lastCards.length;
-alert('Al momento abbiamo ' + numeroViaggi + ' viaggi disponibili')
 
+// Creazione alert
+let numeroViaggi = viaggiSaldi.length + viaggiSummer.length + lastCards.length;
+// alert('Al momento abbiamo ' + numeroViaggi + ' viaggi disponibili') Commentato perché da fastidio
+
+// Funzione che nasconde le cards
 const hideCards = function(){
-    
+    for(let viaggi of viaggiSaldi){
+        viaggi.classList.add('d-none');
+    }
+    for(let viaggi of viaggiSummer){
+        viaggi.classList.add('d-none');
+    }
+    for(let viaggi of lastCards){
+        viaggi.classList.add('d-none')
+    }
 }
+// hideCards();
 
 // Creazione 'hot' badge
-
 let images = document.querySelectorAll('#summerCollapse .card');
-
 for(let image of images){
     image.classList.add('position-relative')
     let span = document.createElement('span');
