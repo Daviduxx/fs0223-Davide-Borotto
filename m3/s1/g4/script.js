4e1009be045f05caff66843cbcf71ac9ac115c6e
Daviduxx <borotto.davide@gmail.com>
2023-05-25 15:03:11 +0200

    classe

--- m3/s1/g4/script.js
+++ m3/s1/g4/script.js
@@ -1,3 +1,4 @@
+var arrayNegozio = [];
 fetch('Abbigliamento.json')
     .then(function (res) {
     console.log(res);
@@ -11,3 +12,19 @@ fetch('Abbigliamento.json')
     .then(function (data) {
     console.log(data);
 });
+var Negozio = /** @class */ (function () {
+    function Negozio(id, codprod, collezione, capo, modello, quantita, colore, prezzoivaesclusa, prezzoivainclusa, disponibile, saldo) {
+        this.id = id,
+            this.codprod = codprod,
+            this.collezione = collezione,
+            this.capo = capo,
+            this.modello = modello,
+            this.quantita = quantita,
+            this.colore = colore,
+            this.prezzoivaesclusa = prezzoivaesclusa,
+            this.prezzoivainclusa = prezzoivainclusa,
+            this.disponibile = disponibile,
+            this.saldo = saldo;
+    }
+    return Negozio;
+}());
