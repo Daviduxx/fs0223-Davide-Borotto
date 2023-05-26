d9bf9cc2a8637c7ce4acb135e9a09c6068a24daa
Daviduxx <borotto.davide@gmail.com>
2023-05-26 14:41:11 +0200

    primo metodo extra

--- m3/s1/g5/script.js
+++ m3/s1/g5/script.js
@@ -22,9 +22,12 @@ class Smartphone {
     getNumeroChiamate() {
         return this.numeroChiamate;
     }
-    chiamata(min) {
+    chiamata(min, durata, year) {
         this.numeroChiamate++;
         this.carica -= (min * this.costoMinuto);
+        //EXTRA
+        let nuovaChiamata = { id: this.registro.length, durata: durata, timing: year };
+        this.registro.push(nuovaChiamata);
     }
     azzeraChiamate() {
         this.numeroChiamate = 0;
@@ -44,39 +47,39 @@ smartphone1.ricarica(8);
 console.log('CARICA AGGIORNATA:', smartphone1.carica);
 console.log(smartphone1.numero404());
 console.log('CHIAMATE EFFETTUATE:', smartphone1.getNumeroChiamate());
-smartphone1.chiamata(5);
+smartphone1.chiamata(5, 4, new Date('2023'));
 console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone1.numeroChiamate, 'CARICA AGGIORNATA:', smartphone1.carica);
 smartphone1.azzeraChiamate();
 console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone1.numeroChiamate);
-smartphone1.chiamata(10);
+smartphone1.chiamata(10, 3, new Date('2023'));
 console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone1.numeroChiamate, 'CARICA AGGIORNATA:', smartphone1.carica);
 console.log(smartphone1.mostraRegistroChiamate());
-console.log('------------------- ISTANZA N.2 ----------------------------');
-let smartphone2 = new Smartphone(10, 4, 0, 2, new Date());
-console.log('SMARTPHONE 2:', smartphone2);
-console.log('CARICA INIZIALE', smartphone2.carica);
-smartphone2.ricarica(5);
-console.log('CARICA AGGIORNATA:', smartphone2.carica);
-console.log(smartphone2.numero404());
-console.log('CHIAMATE EFFETTUATE:', smartphone2.getNumeroChiamate());
-smartphone2.chiamata(5);
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
-smartphone2.azzeraChiamate();
-console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone2.numeroChiamate);
-smartphone2.chiamata(10);
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
-console.log('------------------- ISTANZA N.3 ----------------------------');
-let smartphone3 = new Smartphone(13, 23, 0, 2, new Date());
-console.log('SMARTPHONE 3:', smartphone3);
-console.log('CARICA INIZIALE', smartphone3.carica);
-smartphone3.ricarica(7);
-console.log('CARICA AGGIORNATA:', smartphone3.carica);
-console.log(smartphone3.numero404());
-console.log('CHIAMATE EFFETTUATE:', smartphone3.getNumeroChiamate());
-smartphone3.chiamata(7);
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
-smartphone3.azzeraChiamate();
-console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone3.numeroChiamate);
-smartphone3.chiamata(3);
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
+// console.log('------------------- ISTANZA N.2 ----------------------------');
+// let smartphone2 = new Smartphone(10, 4, 0, 2, new Date())
+// console.log('SMARTPHONE 2:', smartphone2);
+// console.log('CARICA INIZIALE',smartphone2.carica);
+// smartphone2.ricarica(5)
+// console.log('CARICA AGGIORNATA:', smartphone2.carica);
+// console.log(smartphone2.numero404());
+// console.log('CHIAMATE EFFETTUATE:', smartphone2.getNumeroChiamate());
+// smartphone2.chiamata(5)
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
+// smartphone2.azzeraChiamate()
+// console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone2.numeroChiamate);
+// smartphone2.chiamata(10)
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
+// console.log('------------------- ISTANZA N.3 ----------------------------');
+// let smartphone3 = new Smartphone(13, 23, 0, 2, new Date())
+// console.log('SMARTPHONE 3:', smartphone3);
+// console.log('CARICA INIZIALE',smartphone3.carica);
+// smartphone3.ricarica(7)
+// console.log('CARICA AGGIORNATA:', smartphone3.carica);
+// console.log(smartphone3.numero404());
+// console.log('CHIAMATE EFFETTUATE:', smartphone3.getNumeroChiamate());
+// smartphone3.chiamata(7)
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
+// smartphone3.azzeraChiamate()
+// console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone3.numeroChiamate);
+// smartphone3.chiamata(3)
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
 //# sourceMappingURL=script.js.map
--- m3/s1/g5/script.ts
+++ m3/s1/g5/script.ts
@@ -15,7 +15,7 @@ interface iSmartphone{
     ricarica(euro:number):void;
     numero404():string;
     getNumeroChiamate():number;
-    chiamata(min:number):void;
+    chiamata(min:number,durata:number,year:Date):void;
     azzeraChiamate():void;
     mostraRegistroChiamate():void;
     filtraChiamatePerDataOra():void;
@@ -54,9 +54,13 @@ class Smartphone implements iSmartphone {
         return this.numeroChiamate;
     }
 
-    chiamata(min: number): void {
+    chiamata(min: number, durata:number, year:Date): void {
         this.numeroChiamate++;
         this.carica -= (min * this.costoMinuto)
+
+        //EXTRA
+        let nuovaChiamata:{ id:number, durata:number, timing:Date } = {id: this.registro.length, durata: durata, timing: year}
+        this.registro.push(nuovaChiamata)
     }
 
     azzeraChiamate(): void {
@@ -89,68 +93,69 @@ console.log(smartphone1.numero404());
 
 console.log('CHIAMATE EFFETTUATE:', smartphone1.getNumeroChiamate());
 
-smartphone1.chiamata(5)
+smartphone1.chiamata(5, 4, new Date('2023'))
 
 console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone1.numeroChiamate, 'CARICA AGGIORNATA:', smartphone1.carica);
 
 smartphone1.azzeraChiamate()
 console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone1.numeroChiamate);
 
-smartphone1.chiamata(10)
+smartphone1.chiamata(10, 3, new Date('2023'))
 
 console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone1.numeroChiamate, 'CARICA AGGIORNATA:', smartphone1.carica);
 
 console.log(smartphone1.mostraRegistroChiamate());
 
-console.log('------------------- ISTANZA N.2 ----------------------------');
 
-let smartphone2 = new Smartphone(10, 4, 0, 2, new Date())
-console.log('SMARTPHONE 2:', smartphone2);
-console.log('CARICA INIZIALE',smartphone2.carica);
+// console.log('------------------- ISTANZA N.2 ----------------------------');
 
-smartphone2.ricarica(5)
-console.log('CARICA AGGIORNATA:', smartphone2.carica);
+// let smartphone2 = new Smartphone(10, 4, 0, 2, new Date())
+// console.log('SMARTPHONE 2:', smartphone2);
+// console.log('CARICA INIZIALE',smartphone2.carica);
 
-console.log(smartphone2.numero404());
+// smartphone2.ricarica(5)
+// console.log('CARICA AGGIORNATA:', smartphone2.carica);
 
-console.log('CHIAMATE EFFETTUATE:', smartphone2.getNumeroChiamate());
+// console.log(smartphone2.numero404());
 
-smartphone2.chiamata(5)
+// console.log('CHIAMATE EFFETTUATE:', smartphone2.getNumeroChiamate());
 
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
+// smartphone2.chiamata(5)
 
-smartphone2.azzeraChiamate()
-console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone2.numeroChiamate);
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
 
-smartphone2.chiamata(10)
+// smartphone2.azzeraChiamate()
+// console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone2.numeroChiamate);
 
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
+// smartphone2.chiamata(10)
+
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone2.numeroChiamate, 'CARICA AGGIORNATA:', smartphone2.carica);
 
 
 
-console.log('------------------- ISTANZA N.3 ----------------------------');
+// console.log('------------------- ISTANZA N.3 ----------------------------');
 
-let smartphone3 = new Smartphone(13, 23, 0, 2, new Date())
-console.log('SMARTPHONE 3:', smartphone3);
-console.log('CARICA INIZIALE',smartphone3.carica);
+// let smartphone3 = new Smartphone(13, 23, 0, 2, new Date())
+// console.log('SMARTPHONE 3:', smartphone3);
+// console.log('CARICA INIZIALE',smartphone3.carica);
 
-smartphone3.ricarica(7)
-console.log('CARICA AGGIORNATA:', smartphone3.carica);
+// smartphone3.ricarica(7)
+// console.log('CARICA AGGIORNATA:', smartphone3.carica);
 
-console.log(smartphone3.numero404());
+// console.log(smartphone3.numero404());
 
-console.log('CHIAMATE EFFETTUATE:', smartphone3.getNumeroChiamate());
+// console.log('CHIAMATE EFFETTUATE:', smartphone3.getNumeroChiamate());
 
-smartphone3.chiamata(7)
+// smartphone3.chiamata(7)
 
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
 
-smartphone3.azzeraChiamate()
-console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone3.numeroChiamate);
+// smartphone3.azzeraChiamate()
+// console.log('CHIAMATE EFFETTUATE AGGIORNATO:', smartphone3.numeroChiamate);
 
-smartphone3.chiamata(3)
+// smartphone3.chiamata(3)
 
-console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
+// console.log('NUMERO CHIAMATE AGGIORNATO:', smartphone3.numeroChiamate, 'CARICA AGGIORNATA:', smartphone3.carica);
 
 
 
