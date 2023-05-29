7baa19c5a071efbd0cc8058b9becabceeba074a5
Daviduxx <borotto.davide@gmail.com>
2023-05-29 16:55:55 +0200

    final version

--- m3/s2/g1/progetto1/src/app/app.module.ts
+++ m3/s2/g1/progetto1/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { Comp3Component } from './comp3/comp3.component';
+import { MenuComponent } from './menu/menu.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Comp1Component,
     Comp2Component,
-    Comp3Component
+    Comp3Component,
+    MenuComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
