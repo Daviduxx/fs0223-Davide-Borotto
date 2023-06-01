303d830bf91464ed2bbd4234078655e02c3cf82c
Daviduxx <borotto.davide@gmail.com>
2023-06-01 22:45:34 +0200

    get ok

--- m3/s2/g5/todolist/src/app/pages/todos/todos.component.ts
+++ m3/s2/g5/todolist/src/app/pages/todos/todos.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITodo } from 'src/app/Models/i-todo';
+import { TodolistService } from 'src/app/todolist.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+
+  allTodos:ITodo[] = [];
+  constructor(private todoSVC:TodolistService){}
+
+  ngOnInit(){
+
+    this.todoSVC.getTodos().then(todo => this.allTodos = todo);
+
+  }
 
 }
--- m3/s2/g5/todolist/src/app/todolist.service.ts
+++ m3/s2/g5/todolist/src/app/todolist.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ITodo } from './Models/i-todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodolistService {
 
+  APIURL:string = 'http://localhost:3000/todo';
+
   constructor() { }
+
+  getTodos():Promise<ITodo[]>{
+    return fetch(this.APIURL).then(res => res.json())
+  }
+
+  addTodo(todo:ITodo):Promise<ITodo>{
+    return fetch(this.APIURL, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(todo)
+    }).then(res => res.json())
+  }
 }
