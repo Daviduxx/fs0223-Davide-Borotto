ee936c025d298ca38fd71e44d35b833ca21315fa
Daviduxx <borotto.davide@gmail.com>
2023-06-05 19:28:56 +0200

    crud

--- m3/s3/g1/progetto5/src/app/post.service.ts
+++ m3/s3/g1/progetto5/src/app/post.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Post } from './post';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,18 @@ export class PostService {
 
   APIURL:string = 'https://jsonplaceholder.typicode.com/photos';
 
-  constructor() { }
+  constructor( private http:HttpClient) { }
+
+  getPhotos(){
+    return this.http.get<Post[]>(this.APIURL)
+  }
+
+  // RICHIESTA POST CHE ORA NON SERVE
+  // postPost(){
+  //   return this.http.post<Post[]>(this.APIURL)
+  // }
+
+  deletePhoto(id:number){
+    return this.http.delete(this.APIURL + '/' + id)
+  }
 }
--- m3/s3/g1/progetto5/src/app/post/post.component.ts
+++ m3/s3/g1/progetto5/src/app/post/post.component.ts
@@ -1,5 +1,7 @@
+import { PostService } from './../post.service';
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -7,14 +9,27 @@ import { Post } from '../post';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent{
+export class PostComponent implements OnInit{
 
+  constructor( private PostService:PostService) {}
 
   postArr:Post[] = [];
+  LikedArr:Post[] = [];
 
-  constructor() {}
-
-  OnInit(){
-
+  ngOnInit(): void {
+    this.PostService.getPhotos().subscribe(res => {
+      this.postArr = res.filter((p) => p.id < 10)
+    })
   }
+
+  addToFavourite(photo:Post){
+    this.LikedArr.push(photo);
+    console.log(this.LikedArr);
+  }
+
+  delete(id:number){
+    this.PostService.deletePhoto(id).subscribe()
+  }
+
+
 }
