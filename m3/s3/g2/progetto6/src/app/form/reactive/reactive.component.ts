3059f87371d9a6ba7a1a480a32df7aa51f72c56a
Daviduxx <borotto.davide@gmail.com>
2023-06-06 23:24:56 +0200

    reactive form

--- m3/s3/g2/progetto6/src/app/form/reactive/reactive.component.ts
+++ m3/s3/g2/progetto6/src/app/form/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -8,12 +8,25 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ReactiveComponent implements OnInit{
 
-  form!:FormGroup
+  form!:FormGroup;
+  powers:string[] = ['imparare typescript', 'imparare angular', 'imparare react', 'imparare java']
 
   constructor(private fb:FormBuilder){}
 
   ngOnInit(){
 
+    this.form = this.fb.group({
+      nome: this.fb.control(null, Validators.required),
+      alterEgo: this.fb.control(null, Validators.required),
+      power: this.fb.control(null),
+      enemy: this.fb.control(null, Validators.maxLength(10)),
+      planet: this.fb.control(null, [Validators.required, Validators.minLength(5)]),
+      weakness: this.fb.control(null, Validators.required)
+    })
+  }
+
+  onSubmit(){
+    console.log(this.form);
 
   }
 
