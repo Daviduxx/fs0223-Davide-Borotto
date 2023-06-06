39b097e442090280272c32b71ecb941c983163f0
Daviduxx <borotto.davide@gmail.com>
2023-06-06 16:24:36 +0200

    template driven pt.2

--- m3/s3/g2/progetto6/src/app/app.module.ts
+++ m3/s3/g2/progetto6/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateDrivenComponent } from './form/template-driven/template-driven.component';
 import { ReactiveComponent } from './form/reactive/reactive.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ReactiveComponent } from './form/reactive/reactive.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- m3/s3/g2/progetto6/src/app/form/template-driven/template-driven.component.ts
+++ m3/s3/g2/progetto6/src/app/form/template-driven/template-driven.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./template-driven.component.scss']
 })
 export class TemplateDrivenComponent {
+
+  // non è obbligatorio, è semplicemente un modo diverso per accedere al form, in questo caso lato ts
+  @ViewChild('form') form!:NgForm
+
+
+  onSubmit(form:NgForm){
+    // accedo al form lato html
+    console.log(form);
+    console.log(form.form.value);
+
+    // Metodo built in di ng per svuotare il form al submit
+    this.form.reset()
+  }
 
 }
