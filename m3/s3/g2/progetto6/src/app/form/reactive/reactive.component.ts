8f1faa3e5c9671236cece9c472a0a891dfd7740e
Daviduxx <borotto.davide@gmail.com>
2023-06-06 17:00:51 +0200

    reactive form

--- m3/s3/g2/progetto6/src/app/form/reactive/reactive.component.ts
+++ m3/s3/g2/progetto6/src/app/form/reactive/reactive.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.scss']
 })
-export class ReactiveComponent {
+export class ReactiveComponent implements OnInit{
+
+  form!:FormGroup
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(){
+
+
+  }
 
 }
