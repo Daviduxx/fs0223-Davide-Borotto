3cfbd474b56f4c2d1076aa2acf34beef78d0d1a5
Daviduxx <borotto.davide@gmail.com>
2023-06-10 14:06:22 +0200

    auth guard

--- m3/s3/g5/dashboard/src/app/auth/auth-routing.module.ts
+++ m3/s3/g5/dashboard/src/app/auth/auth-routing.module.ts
@@ -4,12 +4,13 @@ import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard',  canActivate: [AuthGuard], component: DashboardComponent }
 ];
 
 @NgModule({
